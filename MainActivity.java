ad16f49f9d88f9179351c7e997190205153cda1e
ch532 <trustconnect713@gmail.com>
2025-07-08 23:39:09 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -3,14 +3,8 @@ package com.connectgold;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.view.View;
-import android.widget.RelativeLayout;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.gms.ads.*;
-import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
-import com.google.android.gms.ads.rewarded.RewardItem;
-import com.google.android.gms.ads.rewarded.RewardedAd;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,37 +16,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         MobileAds.initialize(this, initializationStatus -> {});
 
-        RelativeLayout layout = new RelativeLayout(this);
-        webView = new WebView(this);
-        bannerAdView = new AdView(this);
-        bannerAdView.setAdSize(AdSize.BANNER);
-        bannerAdView.setAdUnitId("ca-app-pub-1171216593802007/8884489855"); // Banner Ad
+        webView = findViewById(R.id.webView);
+        bannerAdView = findViewById(R.id.adView);
 
-        RelativeLayout.LayoutParams webParams = new RelativeLayout.LayoutParams(
-            RelativeLayout.LayoutParams.MATCH_PARENT,
-            RelativeLayout.LayoutParams.MATCH_PARENT
-        );
-        webParams.addRule(RelativeLayout.ABOVE, bannerAdView.getId());
-
-        webView.setWebViewClient(new WebViewClient());
+        // Enable JavaScript for Umami and WebView
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("https://connectgold.sbs");
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://connectgold.sbs");  // Umami already works here
 
-        layout.addView(webView, webParams);
+        // Load banner ad
+        AdRequest adRequest = new AdRequest.Builder().build();
+        bannerAdView.loadAd(adRequest);
 
-        RelativeLayout.LayoutParams adParams = new RelativeLayout.LayoutParams(
-            RelativeLayout.LayoutParams.MATCH_PARENT,
-            RelativeLayout.LayoutParams.WRAP_CONTENT
-        );
-        adParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-        layout.addView(bannerAdView, adParams);
-
-        setContentView(layout);
-
-        bannerAdView.loadAd(new AdRequest.Builder().build());
+        // Load interstitial and rewarded
         loadInterstitialAd();
         loadRewardedAd();
     }
@@ -60,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     private void loadInterstitialAd() {
         AdRequest adRequest = new AdRequest.Builder().build();
         InterstitialAd.load(this,
-                "ca-app-pub-1171216593802007/4945244849", // Interstitial ID
+                "ca-app-pub-1171216593802007/4945244849",
                 adRequest,
                 new InterstitialAdLoadCallback() {
                     @Override
@@ -74,14 +54,14 @@ public class MainActivity extends AppCompatActivity {
     private void loadRewardedAd() {
         AdRequest adRequest = new AdRequest.Builder().build();
         RewardedAd.load(this,
-                "ca-app-pub-1171216593802007/1435861786", // Rewarded ID
+                "ca-app-pub-1171216593802007/1435861786",
                 adRequest,
                 new RewardedAdLoadCallback() {
                     @Override
                     public void onAdLoaded(RewardedAd ad) {
                         rewardedAd = ad;
                         rewardedAd.show(MainActivity.this, rewardItem -> {
-                            // Reward logic
+                            // Reward logic (optional)
                         });
                     }
                 });
